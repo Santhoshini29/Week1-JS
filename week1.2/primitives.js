5a421fef91b6d5e03ae8d8a1581078bfa3eefe9a
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-12-19 21:46:10 +0530

    added code

---
+++ week1.2/primitives.js
@@ -0,0 +1,37 @@
+//loging in 
+console.log ("hello world");
+
+console.log (a);
+//variable-- var, let ,const 
+  //var -- original way 
+  //let -- mostly use this 
+  //const -- to assign single value 
+  //var-- can change the value no error 
+  var a=1;
+  a=2;
+  console.log(a);
+ //let-- can change the value no error 
+  let a=1;
+  a=2;
+  console.log(a);
+  //const -- it occurs error 
+  const a=1;
+  a=2;
+  console.log(a);
+  //
+  let firstname = "hello";
+let age = 22;
+let ismarried = true;
+
+console.log("this persons name is "+firstname+" and their age is "+age )
+//if else 
+
+if(isMarried == true){
+  console.log(firstName + " is married.");
+}
+else{
+  console.log(firstName + " is not married.");
+}
+
+ 
+  
