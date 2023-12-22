5412c75a0afee815617a541e448ebbb6b1f52a0b
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-12-22 22:44:32 +0530

    added

--- week1.2/primitives.js
+++ week1.2/primitives.js
@@ -51,15 +51,7 @@ for(let i = 0; i<numberOfPeople; i++){
   }
 }
 
-const personArray = ["ram", "sita", "laxman", "hanuman"];
-const genderArray = ["male", "female", "male", "male",];
-const numberOfUsers = personArray.length;
-  // loop through and print all male / female names
-for(let i = 0; i<numberOfUsers; i++){
-      if (genderArray[i] == "male") {
-         console.log(personArray[i]);
-      }
-  }
+
  
   
    
