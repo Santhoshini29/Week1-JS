47c714c5559d432f93ada51a0df1277fbbefcd0e
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-12-25 15:42:53 +0530

    added

--- week1.3/01-String.js
+++ week1.3/01-String.js
@@ -4,5 +4,18 @@ function getLength(str){
   console.log("length:",str.length);
 
 }
-getLength("hello worlD")
+getLength("hello world");
+
+
+// index of 
+ function findIndexof(str,target){
+  console.log("Original string is:",str);
+  console.log("Index  is:",str.last(target));
+
+ }
+ findIndexof("Hello world","world");
+ // str.length
+ //str.lastIndexOf(argument)
+
+ //getSlice - - - - 
  
